4a6daba5491bf3ff8b371e838059e211ae7ee67d
Adisudirta <yanadisudirta@gmail.com>
2022-01-26 01:52:21 -0800

    finished create HeaderApp component

---
+++ components/HeaderApp.vue
@@ -0,0 +1,65 @@
+<script setup lang="ts">
+const isHiden = ref<boolean>(true);
+const isRotate = ref<boolean>(false);
+
+function handleToggle(): void {
+  isHiden.value = !isHiden.value;
+  isRotate.value = !isRotate.value;
+}
+</script>
+
+<template>
+  <header
+    class="w-screen bg-gray-900 flex justify-between items-center fixed px-10 py-4 sm:px-20 md:px-28"
+  >
+    <router-link to="/" class="flex items-center gap-3">
+      <img class="w-7" src="../assets/images/joystick.png" alt="logo" />
+      <h1 class="font-bold text-gray-400 text-lg">GAMES STORE</h1>
+    </router-link>
+
+    <!-- Desktop -->
+    <div class="hidden md:flex gap-5">
+      <NuxtLink to="/Category" class="text-gray-500 font-bold text-sm">
+        Category
+      </NuxtLink>
+      <NuxtLink to="/Platform" class="text-gray-500 font-bold text-sm">
+        Platform
+      </NuxtLink>
+    </div>
+
+    <!-- Mobile nav and Tablet nav -->
+    <div
+      class="absolute bg-gray-700 top-[15vh] p-10 flex flex-col justify-center items-center gap-5 w-3/4 rounded-md"
+      :class="{ hidden: isHiden }"
+    >
+      <router-link to="/Category" class="text-gray-500 font-bold">
+        Category
+      </router-link>
+      <router-link to="/Platform" class="text-gray-500 font-bold">
+        Platform
+      </router-link>
+    </div>
+
+    <!-- Toggle button -->
+    <div
+      class="transition-all ease-in text-white block md:hidden"
+      :class="{ 'rotate-90': isRotate }"
+      @click="handleToggle"
+    >
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        class="h-6 w-6"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke="currentColor"
+      >
+        <path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          stroke-width="2"
+          d="M4 6h16M4 12h16M4 18h16"
+        />
+      </svg>
+    </div>
+  </header>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,7 @@
 <template>
-  <div class="container mx-auto bg-gray-300 p-8">
-    <h1 class="font-bold text-gray-600 text-lg">Hello, Tailwind 3!</h1>
-  </div>
+  <Html>
+    <Link rel="icon" href="../assets/images/favicon.png"></Link>
+    <Title>Games Store</Title>
+  </Html>
+  <HeaderApp />
 </template>
